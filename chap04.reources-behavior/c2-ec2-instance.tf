ad75429cfcacf50c1380ad6df6ae8894109593a2
Brahim MISSAOUI <designre.devops@gmail.com>
2022-04-22 18:22:25 +0200

    feat(resource): destroy and recreate resource

--- chap04.reources-behavior/c2-ec2-instance.tf
+++ chap04.reources-behavior/c2-ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "designre-ec2-instance" {
 
   ami  = "ami-04a7df89e71c1c3c6"
   instance_type = "t2.micro"
-  availability_zone = "eu-west-3a"
+  availability_zone = "eu-west-3b"
   tags = {
     "Name" = "web"
      "newtag" = "designre-new-tag"
